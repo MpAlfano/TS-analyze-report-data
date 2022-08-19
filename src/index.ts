525d6bc28ef522d33cdab76ad87aadc8d7ca7b24
Mark Alfano <mpalfano4@gmail.com>
2022-08-19 16:40:27 -0400

    csvFileReader class created

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("testing");
+import { CsvFileReader } from "./CsvFileReader";
+
+const reader = new CsvFileReader("data.csv");
+reader.read();
+
+// enum - enumeration: object storing very closely related values
+enum MatchResult {
+  HomeWin = "H",
+  AwayWin = "A",
+  Draw = "D",
+}
+let manUnitedWins = 0;
+
+for (let match of reader.data) {
+  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games`);
