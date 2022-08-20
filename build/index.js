2bc632e3877a6737e5fea0affbfe5b077ee21ea0
Mark Alfano <mpalfano4@gmail.com>
2022-08-19 23:23:41 -0400

    Delete build directory

--- build/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const MatchReader_1 = require("./MatchReader");
-const Summary_1 = require("./Summary");
-const matchReader = MatchReader_1.MatchReader.fromCsv("data.csv");
-const summary = Summary_1.Summary.winsAnalylsisHtml("Man United");
-matchReader.load();
-summary.buildAndPrintReport(matchReader.matches);
